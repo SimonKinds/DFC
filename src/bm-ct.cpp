ceb9970c0c9549c42e05d2c2c0fa5c9e184af972
Simon Kindström <simon.kindstrom@gmail.com>
2018-06-27 12:43:51 +0200

    Added another CT indexing benchmark

--- src/bm-ct.cpp
+++ src/bm-ct.cpp
@@ -1,5 +1,6 @@
-#include "benchmark/benchmark.h"
+#include <cstdint>
 
+#include "benchmark/benchmark.h"
 #include "ct-initializer.hpp"
 
 namespace {
@@ -34,4 +35,24 @@ void CT_OneByte_ExactMatching(benchmark::State& state) {
 }
 
 BENCHMARK(CT_OneByte_ExactMatching);
+
+void CT_FourByte_ExactMatching(benchmark::State& state) {
+  auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
+
+  dfc::CompactTableInitializer<uint32_t, 49157, 0x20000> initializer;
+  std::string patternValue("abcd");
+  benchmark::DoNotOptimize(&patternValue);
+
+  patterns->emplace_back(0, pattern(patternValue.data()));
+  int const patternIndex = 0;
+  initializer.addPattern(patternIndex, patterns->at(patternIndex));
+
+  auto const ct = initializer.ct<TestOnMatcher>(patterns);
+
+  for (auto _ : state) {
+    ct.exactMatching(patternValue.data(), 4);
+  }
+}
+
+BENCHMARK(CT_FourByte_ExactMatching);
 }  // namespace
