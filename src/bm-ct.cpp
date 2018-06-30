50a6d169f179bb272f7f5ff9ce5a83063b7c704f
Simon Kindström <simon.kindstrom@gmail.com>
2018-06-30 09:40:45 +0200

    Added different lengths for benchmark test

--- src/bm-ct.cpp
+++ src/bm-ct.cpp
@@ -30,7 +30,7 @@ void CT_OneByte_ExactMatching(benchmark::State& state) {
   int const patternIndex = 0;
   initializer.addPattern(patternIndex, patterns->at(patternIndex));
 
-  auto const ct = initializer.ct<TestOnMatcher>(patterns);
+  auto const ct = initializer.ct<TestOnMatcher, dfc::MemcmpMatcher>(patterns);
 
   for (auto _ : state) {
     ct.exactMatching(patternValue.data(), 1);
@@ -38,18 +38,18 @@ void CT_OneByte_ExactMatching(benchmark::State& state) {
 }
 BENCHMARK(CT_OneByte_ExactMatching);
 
-void CT_FourByte_ExactMatching(benchmark::State& state) {
+void CT_FourByte_ExactMatching_Memcmp(benchmark::State& state) {
   auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
 
   CTInitializerFourByteIndexer initializer;
-  std::string patternValue("abcd");
+  std::string patternValue(state.range(0), 'a');
   benchmark::DoNotOptimize(&patternValue);
 
   patterns->emplace_back(0, pattern(patternValue.data()));
   int const patternIndex = 0;
   initializer.addPattern(patternIndex, patterns->at(patternIndex));
 
-  auto const ct = initializer.ct<TestOnMatcher>(patterns);
+  auto const ct = initializer.ct<TestOnMatcher, dfc::MemcmpMatcher>(patterns);
 
   byte const* data = reinterpret_cast<byte const*>(patternValue.data());
   int size = patternValue.size();
@@ -60,33 +60,7 @@ void CT_FourByte_ExactMatching(benchmark::State& state) {
     ct.exactMatching(data, size);
   }
 }
-
-BENCHMARK(CT_FourByte_ExactMatching);
-
-void CT_FourByte_ExactMatching_LongPattern(benchmark::State& state) {
-  auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
-
-  CTInitializerFourByteIndexer initializer;
-  std::string patternValue(64, 'a');
-
-  patterns->emplace_back(0, pattern(patternValue.data()));
-  int const patternIndex = 0;
-  initializer.addPattern(patternIndex, patterns->at(patternIndex));
-
-  auto const ct = initializer.ct<TestOnMatcher>(patterns);
-
-  byte const* data = reinterpret_cast<byte const*>(patternValue.data());
-  int size = patternValue.size();
-
-  benchmark::DoNotOptimize(&data);
-  benchmark::DoNotOptimize(&size);
-
-  for (auto _ : state) {
-    ct.exactMatching(data, size);
-  }
-}
-
-BENCHMARK(CT_FourByte_ExactMatching_LongPattern);
+BENCHMARK(CT_FourByte_ExactMatching_Memcmp)->Range(4, 1024);
 
 void CT_OneByte_ExactMatching_Loop(benchmark::State& state) {
   auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
@@ -111,7 +85,7 @@ void CT_FourByte_ExactMatching_Loop(benchmark::State& state) {
   auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
 
   CTInitializerFourByteIndexer initializer;
-  std::string patternValue("abcd");
+  std::string patternValue(state.range(0), 'a');
   benchmark::DoNotOptimize(&patternValue);
 
   patterns->emplace_back(0, pattern(patternValue.data()));
@@ -130,30 +104,5 @@ void CT_FourByte_ExactMatching_Loop(benchmark::State& state) {
   }
 }
 
-BENCHMARK(CT_FourByte_ExactMatching_Loop);
-
-void CT_FourByte_ExactMatching_LongPattern_Loop(benchmark::State& state) {
-  auto patterns = std::make_shared<std::vector<dfc::Pattern>>();
-
-  CTInitializerFourByteIndexer initializer;
-  std::string patternValue(64, 'a');
-
-  patterns->emplace_back(0, pattern(patternValue.data()));
-  int const patternIndex = 0;
-  initializer.addPattern(patternIndex, patterns->at(patternIndex));
-
-  auto const ct = initializer.ct<TestOnMatcher, dfc::LoopMatcher>(patterns);
-
-  byte const* data = reinterpret_cast<byte const*>(patternValue.data());
-  int size = patternValue.size();
-
-  benchmark::DoNotOptimize(&data);
-  benchmark::DoNotOptimize(&size);
-
-  for (auto _ : state) {
-    ct.exactMatching(data, size);
-  }
-}
-
-BENCHMARK(CT_FourByte_ExactMatching_LongPattern_Loop);
+BENCHMARK(CT_FourByte_ExactMatching_Loop)->Range(4, 1024);
 }  // namespace
