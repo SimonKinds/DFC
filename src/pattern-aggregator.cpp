38e22fc5035504df6a105e620b880663b96a6c51
Simon Kindström <simon.kindstrom@gmail.com>
2018-07-18 16:50:08 +0200

    Moved PatternAggregator impl. to .cpp

---
+++ src/pattern-aggregator.cpp
@@ -0,0 +1,32 @@
+#include "pattern-aggregator.hpp"
+
+namespace dfc {
+void PatternAggregator::add(RawPattern pat) {
+  patterns_.emplace_back(std::move(pat));
+}
+
+std::vector<ImmutablePattern> PatternAggregator::aggregate() {
+  removeDuplicates();
+
+  return createPatterns();
+}
+
+void PatternAggregator::removeDuplicates() {
+  std::sort(std::begin(patterns_), std::end(patterns_));
+  patterns_.erase(std::unique(std::begin(patterns_), std::end(patterns_)),
+                  std::end(patterns_));
+}
+
+std::vector<ImmutablePattern> PatternAggregator::createPatterns() const {
+  std::vector<ImmutablePattern> newPatterns;
+  newPatterns.reserve(patterns_.size());
+
+  Pid pid = 0;
+  for (auto&& pattern : patterns_) {
+    newPatterns.emplace_back(pid, std::move(pattern));
+    ++pid;
+  }
+
+  return newPatterns;
+}
+}  // namespace dfc
--- src/pattern-aggregator.hpp
+++ src/pattern-aggregator.hpp
@@ -12,39 +12,12 @@ class PatternAggregator {
   std::vector<RawPattern> patterns_;
 
  public:
-  void add(RawPattern pat) { patterns_.emplace_back(std::move(pat)); }
-
-  std::vector<ImmutablePattern> aggregate() {
-    removeDuplicates();
-
-    auto patterns = createPatterns();
-
-    resetPatterns();
-
-    return patterns;
-  }
+  void add(RawPattern pat);
+  std::vector<ImmutablePattern> aggregate();
 
  private:
-  void removeDuplicates() {
-    std::sort(std::begin(patterns_), std::end(patterns_));
-    patterns_.erase(std::unique(std::begin(patterns_), std::end(patterns_)),
-                    std::end(patterns_));
-  }
-
-  std::vector<ImmutablePattern> createPatterns() {
-    std::vector<ImmutablePattern> newPatterns;
-    newPatterns.reserve(patterns_.size());
-
-    Pid pid = 0;
-    for (auto&& pattern : patterns_) {
-      newPatterns.emplace_back(pid, std::move(pattern));
-      ++pid;
-    }
-
-    return newPatterns;
-  }
-
-  void resetPatterns() { patterns_.clear(); }
+  void removeDuplicates();
+  std::vector<ImmutablePattern> createPatterns() const;
 };
 }  // namespace dfc
 
