eb96dab30a4dbb0fcab66bf75750d347a663d52a
Simon Kindström <simon.kindstrom@gmail.com>
2018-07-30 13:26:25 +0200

    Use array instead of vector for segment extender

--- src/segment-extender.hpp
+++ src/segment-extender.hpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <cstring>
 #include <limits>
+#include <optional>
 #include <vector>
 
 #include "byte.hpp"
@@ -16,8 +17,18 @@ class SegmentExtender {
   // if we allow 2 bytes, we're already at 16k permutations
   static int const allowedBytesToExtend = 1;
 
+  static constexpr int permutationCount() noexcept {
+    return characterValueCount();
+  }
+
+  static constexpr int characterValueCount() noexcept {
+    return std::numeric_limits<uint8_t>::max() + 1;
+  }
+
  public:
   using SegmentPermutation = std::array<byte, segmentSize>;
+  using PermutationCollection =
+      std::array<SegmentPermutation, permutationCount()>;
 
   struct TooManyPermutationsException : public std::exception {
     char const* what() const throw() {
@@ -26,7 +37,7 @@ class SegmentExtender {
     }
   };
 
-  std::vector<SegmentPermutation> extend(Pattern const& pattern) const {
+  PermutationCollection extend(Pattern const& pattern) const {
     if (!isAllowed(pattern)) {
       throw TooManyPermutationsException();
     }
@@ -48,20 +59,15 @@ class SegmentExtender {
     return segmentSize - pattern.size() <= allowedBytesToExtend;
   }
 
-  std::vector<SegmentPermutation> initializePermutationContainer(
+  PermutationCollection initializePermutationContainer(
       Pattern const& pattern) const {
     SegmentPermutation permutation;
     std::memcpy(permutation.data(), pattern.data(), pattern.size());
-    std::vector<SegmentPermutation> permutations(permutationCount(),
-                                                 permutation);
+
+    PermutationCollection permutations;
+    std::fill(std::begin(permutations), std::end(permutations), permutation);
 
     return permutations;
-  }
-
-  static int permutationCount() noexcept { return characterValueCount(); }
-
-  static int characterValueCount() noexcept {
-    return std::numeric_limits<uint8_t>::max() + 1;
   }
 };
 }  // namespace dfc
