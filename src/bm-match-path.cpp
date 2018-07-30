2b2d1a771287392bc8e835ca18d038f6c66153d3
Simon Kindström <simon.kindstrom@gmail.com>
2018-07-30 11:53:35 +0200

    Added benchmarks for 2B DF + 4B CT for match path

--- src/bm-match-path.cpp
+++ src/bm-match-path.cpp
@@ -14,6 +14,9 @@ using dfc::test::createImmutablePattern;
 using TwoByteMatchPath =
     dfc::MatchPath<PatternRange<2, 100>, DirectFilter<uint16_t>,
                    CompactTable<uint16_t, 1, 0x100>>;
+using TwoByteDfFourByteCtMatchPath =
+    dfc::MatchPath<PatternRange<4, 100>, DirectFilter<uint16_t>,
+                   CompactTable<uint32_t, 49157, 0x20000>>;
 
 namespace {
 void MatchPath_TwoByte_NoHit(benchmark::State& state) {
@@ -43,5 +46,33 @@ void MatchPath_TwoByte_Hit(benchmark::State& state) {
   }
 }
 BENCHMARK(MatchPath_TwoByte_Hit);
+
+void MatchPath_FourByte_NoHit(benchmark::State& state) {
+  TwoByteDfFourByteCtMatchPath path;
+
+  dfc::benchmark::CountOnMatcher onMatcher;
+
+  for (auto _ : state) {
+    path.match("abab", 2, onMatcher);
+    int count = onMatcher.matchCount;
+    benchmark::DoNotOptimize(count);
+    benchmark::ClobberMemory();
+  }
+}
+BENCHMARK(MatchPath_FourByte_NoHit);
+
+void MatchPath_FourByte_Hit(benchmark::State& state) {
+  TwoByteDfFourByteCtMatchPath path;
+  path.addPattern(createImmutablePattern(0, "abab"));
+
+  dfc::benchmark::CountOnMatcher onMatcher;
+  for (auto _ : state) {
+    path.match("abab", 2, onMatcher);
+    int count = onMatcher.matchCount;
+    benchmark::DoNotOptimize(count);
+    benchmark::ClobberMemory();
+  }
+}
+BENCHMARK(MatchPath_FourByte_Hit);
 
 }  // namespace
