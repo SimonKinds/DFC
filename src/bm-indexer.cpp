c2e85cbec5cce1d75defb5d834a00d45ef5afc02
Simon Kindström <simon.kindstrom@gmail.com>
2018-09-21 13:36:25 +0200

    Removed unnecessary static in unnamed namespace

--- src/bm-indexer.cpp
+++ src/bm-indexer.cpp
@@ -10,7 +10,7 @@ using TwoByteDirectFilterIndexer =
 using FourByteHashDirectFilterIndexer =
     dfc::DirectFilterIndexer<uint32_t, 4909, uint16_t>;
 
-static void DF_TwoByte_Indexer(benchmark::State& state) {
+void DF_TwoByte_Indexer(benchmark::State& state) {
   uint16_t segment = std::numeric_limits<uint16_t>::max();
   benchmark::DoNotOptimize(&segment);
   TwoByteDirectFilterIndexer indexer;
@@ -20,7 +20,7 @@ static void DF_TwoByte_Indexer(benchmark::State& state) {
 }
 BENCHMARK(DF_TwoByte_Indexer);
 
-static void DF_FourByteHash_Indexer(benchmark::State& state) {
+void DF_FourByteHash_Indexer(benchmark::State& state) {
   uint32_t segment = std::numeric_limits<uint32_t>::max();
   benchmark::DoNotOptimize(&segment);
   FourByteHashDirectFilterIndexer indexer;
@@ -30,7 +30,7 @@ static void DF_FourByteHash_Indexer(benchmark::State& state) {
 }
 BENCHMARK(DF_FourByteHash_Indexer);
 
-static void CT_Small_Indexer(benchmark::State& state) {
+void CT_Small_Indexer(benchmark::State& state) {
   auto segment = std::numeric_limits<uint8_t>::max();
   benchmark::DoNotOptimize(&segment);
   dfc::CompactTableIndexer<uint8_t, 1, 0xff> indexer;
@@ -40,7 +40,7 @@ static void CT_Small_Indexer(benchmark::State& state) {
 }
 BENCHMARK(CT_Small_Indexer);
 
-static void CT_Large_Indexer(benchmark::State& state) {
+void CT_Large_Indexer(benchmark::State& state) {
   auto segment = std::numeric_limits<uint32_t>::max();
   benchmark::DoNotOptimize(&segment);
   dfc::CompactTableIndexer<uint16_t, 44257, 0x1fff> indexer;
